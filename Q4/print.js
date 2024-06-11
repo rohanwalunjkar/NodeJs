03f0218ac063e585385ada632209f71580fe06f5
rohanwalunjkar <walunjkarrohan@gmail.com>
2024-06-11 23:06:20 +0530

    Que_4

---
+++ Q4/print.js
@@ -0,0 +1,29 @@
+function print()
+{   
+    // str=""
+
+    
+    for(i=1;i<=100;i++)
+    {
+        if(i%3==0)
+        {
+            console.log("fizz");
+        }
+        else if(i%5==0)
+        {
+            console.log("buzz");
+        }
+        else if(i%3==0 && i%5==0)
+        {
+            console.log("fizzbuzz");
+        }
+        else
+        {
+            console.log(i);
+        }
+        
+
+    }
+}
+
+print();
